b6ca28311dfff49a03b6618b4aaf3ac0094a66b0
mkem114 <mkem114@aucklanduni.ac.nz>
2016-09-30 20:43:59 +1300

    Quaternary Commit
    -Task 3 skeleton class fully commented

--- src/Task3.java
+++ src/Task3.java
@@ -9,19 +9,38 @@ import java.io.OutputStreamWriter;
 import java.util.Comparator;
 import java.util.LinkedList;
 
+/**
+ * <h1>Task3</h1>
+ * The ultimate solution to Gerald's infamous secondary auto-marker assignment for SE211 S2 2016
+ * <p>
+ * This class takes a graph from stdin, z-sorts it and then prints the result to stdout. See assignment specs for
+ * specifics
+ * </p>
+ */
 public class Task3 {
-    // debug switch
+    /**
+     * Debug-mode printing switch with "true" being on
+     */
     public static final boolean _debug = true;
     // stdin
     private BufferedReader _in;
     // stdout
     private BufferedWriter _out;
 
+    /**
+     * Starts the program after being called from command line
+     * @param args Ignored command line arguments
+     * @throws Exception All exceptions are thrown for debugging as this is not intended for production
+     */
     public static void main(String[] args) throws Exception {
         // Creates new graph solver
         new Task3();
     }
 
+    /**
+     * The main function in which initialisation and general program control flow occurs
+     * @throws Exception All exceptions are thrown for debugging as this is not intended for production
+     */
     private Task3() throws Exception {
         // Initialises the input and output streams respectively
         _in = new BufferedReader(new InputStreamReader(System.in));
@@ -40,16 +59,43 @@ public class Task3 {
         _out.flush();
     }
 
+    /**
+     * Loads the graph from stdin
+     * @throws Exception All exceptions are thrown for debugging as this is not intended for production
+     */
     private void load() throws Exception {
     }
 
+    /**
+     * Solves the loaded graph
+     */
     private void solve() {
     }
 
+    /**
+     * Prints the z-sort solution to stout
+     * @throws Exception All exceptions are thrown for debugging as this is not intended for production
+     */
     private void print() throws Exception {
     }
 
+    /**
+     * <h1>NodeComparer</h1>
+     * <p>
+     * Represents something to compare Nodes
+     * </p>
+     * <p>
+     * Only dependency is Node class
+     * </p>
+     */
     private class NodeComparer implements Comparator<Node> {
+        /**
+         * Same as o1.compareTo(o2) thus see Node.compareTo(Node n)
+         *
+         * @param o1 First Node
+         * @param o2 Second Node
+         * @return Order of nodes
+         */
         @Override
         public int compare(Node o1, Node o2) {
             return o1.compareTo(o2);
@@ -59,23 +105,24 @@ public class Task3 {
     /**
      * <h1>Node</h1>
      * <p>
-     *     Represents a node in a graph (e.g DAG, binary tree, cycle)
+     * Represents a node in a graph (e.g DAG, binary tree, cycle)
      * </p>
      * <p>
-     *     Responsible for storing and determining information about itself
+     * Responsible for storing and determining information about itself including comparision to other nodes
      * </p>
      * <p>
-     *     No dependencies other than built in java libraries
+     * No dependencies other than built in java libraries
      * </p>
      */
     class Node implements Comparable<Node> {
         private int _name;
         private int _strata;
-        private LinkedList<Node> _from;
-        private LinkedList<Node> _to;
+        private LinkedList<Node> _from; // List of nodes the node comes from
+        private LinkedList<Node> _to; // Lists of nodes the node goes to
 
         /**
          * Creates a new node with the given name and assumes 0th strata to begin with
+         *
          * @param name Node's name
          */
         Node(int name) {
@@ -84,12 +131,14 @@ public class Task3 {
 
         /**
          * Creates a new node with a name and strata
-         * @param name Node's name
+         *
+         * @param name   Node's name
          * @param strata Node's strata
          */
         Node(int name, int strata) {
             _name = name;
             _strata = strata;
+            // Initialises the to and from lists
             _from = new LinkedList<>();
             _to = new LinkedList<>();
         }
@@ -99,21 +148,25 @@ public class Task3 {
          * zero means that no difference is found
          * negative means that the specified node is greater
          * positive means that the specified nod is lesser
+         *
          * @param n Specified node to compare
          * @return Order of nodes
          */
         public int compareTo(Node n) {
+            // Checks the strata first
             if (n.strataOn() > _strata) {
                 return -1;
             } else if (n.strataOn() < _strata) {
                 return 1;
             } else {
+                // Checks the name secondly
                 return Integer.compare(_name, n.name());
             }
         }
 
         /**
          * Determines the name of the node
+         *
          * @return Node's name
          */
         public int name() {
@@ -122,22 +175,27 @@ public class Task3 {
 
         /**
          * Informs the node that it is pointing to another (specified) node
+         *
          * @param to Pointed node
          */
         public void goesTo(Node to) {
+            // Adds the node to the to list
             _to.add(to);
         }
 
         /**
          * Informs the node that it is pointed to by another (specified) node
+         *
          * @param from Pointing node
          */
         public void comesFrom(Node from) {
+            // Adds the node to the from list
             _from.add(from);
         }
 
         /**
          * Evaluates what strata number the node is on
+         *
          * @return Nth strata
          */
         public int strataOn() {
@@ -146,6 +204,7 @@ public class Task3 {
 
         /**
          * Evaluates whether on not the node is on the 0th strata to its knowledge
+         *
          * @return Whether or not its on strata 0
          */
         public boolean base() {
